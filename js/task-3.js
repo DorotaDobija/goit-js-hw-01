c02dd219ba869ac6742d4185d3c04ad61724aa4c
DorotaDobija <117933735+DorotaDobija@users.noreply.github.com>
2024-04-23 19:45:31 +0200

    task-3 done

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+function getElementWidth(content, padding, border) {
+    const finalContent = Number(content.replace("px", ""));
+    const finalPadding = Number(padding.replace("px", "") * 2);
+    const finalBorder = Number(border.replace("px", "") * 2);
+
+    return finalContent + finalPadding + finalBorder;
+
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); 
+console.log(getElementWidth("60px", "12px", "8.5px")); 
+console.log(getElementWidth("200px", "0px", "0px")); 
